0401bb3efa5e4ee2a47d740feb5018e6c21b2c70
James Dennis <jd@j2labs.net>
2011-03-29 01:03:57 -0400

    Added ruby example

--- multilang/client.rb
+++ multilang/client.rb
@@ -1,1 +1,14 @@
-puts "\n    Here's a place to start: http://zguide.zeromq.org/rb:hwclient\n"
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'ffi-rzmq'
+
+ctx = ZMQ::Context.new(1)
+s = context.socket(ZMQ::SUB)
+s.connect("ipc://hellostream:5678")
+s.setsockopt(ZMQ::SUBSCRIBE, "hellos")
+
+while 1 > 0
+  reply = requester.recv_string
+  puts "Received: #{reply}"
+end
